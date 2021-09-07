a13ce776f8f6ef94bbf7d290e465ef741426bb8a
YupRTS <62153669+Qwelice@users.noreply.github.com>
2021-09-07 22:03:18 +0300

    issue #1

--- src/issues/Complex.kt
+++ src/issues/Complex.kt
@@ -1,5 +1,25 @@
 package issues
 
-class Complex(val Re: Double, val Im: Double) {
-    
+import java.lang.IllegalArgumentException
+
+class Complex(val re: Double, val im: Double) {
+
+
+    operator fun plus(other: Complex) : Complex{
+        return Complex(re + other.re, im + other.im)
+    }
+
+    // необходимо переопределить должным образом
+    operator fun div(other: Complex) : Complex{
+        if(true){ // необходима проверка на ноль
+            return Complex(0.0, 0.0)
+        }else{
+            // исключение будет выбрасываться, если проверка на ноль не была пройдена
+            throw IllegalArgumentException("Нельзя делить на ноль")
+        }
+    }
+
+    override fun toString(): String {
+        return "Необходимо переопределить должным образом"
+    }
 }
